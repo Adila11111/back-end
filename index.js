e6195ab15ae7fa724b8dcdfb7f6caebf8c318a47
Adila11111 <109820025+Adila11111@users.noreply.github.com>
2025-01-17 10:12:54 +0500

    first

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+// api/index.js
+const express = require('express');
+const app = express();
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 4000
-
+// Sample route 1
 app.get('/', (req, res) => {
-  res.send('Hello Worl!')
-})
+  res.send('Hello World!');
+});
 
+// Sample route 2 (Twitter route)
 app.get('/twitter', (req, res) => {
-    res.send('Adeela..!')
-  })
+  res.send('Adeela..!');
+});
 
-app.get('/login', (req, res)=> {
-    res.send('<h1>please Login at here</h1>')
-})
+// Sample route 3 (Login route)
+app.get('/login', (req, res) => {
+  res.send('<h1>Please Login here</h1>');
+});
 
-app.get('/youtube', (req, res)=> {
-    res.send('<h2>please Login at here</h2>')
-}) 
+// Sample route 4 (YouTube route)
+app.get('/youtube', (req, res) => {
+  res.send('<h2>Please Login here</h2>');
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Export the handler as a Vercel serverless function
+module.exports = app;
